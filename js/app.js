ace9cdf533ec9ebcda7c57242bbfa5d0ed8bb8b8
SantiagoS <santiagosanchezmon@gmail.com>
2022-03-28 20:03:40 -0300

    ultimos cambios

--- js/app.js
+++ js/app.js
@@ -3,18 +3,28 @@ import Animal from "./animal.js";
 import Gato from "./gato.js";
 import Perro from "./perro.js";
 import CardAnimal from "./card.js";
+import Card from "./card.js"
 
 let header = document.querySelector("#myHeader");
 let myHeader = new Header("LoGo");
 header.innerHTML = myHeader.render();
 
-let insertCard1 = document.querySelector(".cards");
+// let insertCard1 = document.querySelector(".cards");
 let card1 = new CardAnimal("./img/tomas.jpg", "Tomás", 12, "Perro");
-insertCard1.innerHTML = card1.newCard();
+// insertCard1.innerHTML = card1.newCard();
 
-let insertCard2 = document.querySelector(".cards");
 let card2 = new CardAnimal("./img/koshka.jpg", "Koshka", 3, "Gato");
-insertCard2.innerHTML = card2.newCard();
+
+card1.newCardMejorada();
+card2.newCardMejorada();
+card1.newCardMejorada();
+card2.newCardMejorada();
+card1.newCardMejorada();
+card2.newCardMejorada();
+
+
+// let insertCard2 = document.querySelector(".cards");
+// insertCard2.innerHTML = card2.newCard();
 
 let gato1 = new Gato("koshca", 3, "mix", "hembra");
 gato1.miau();
--- js/card.js
+++ js/card.js
@@ -22,4 +22,24 @@ export default class CardAnimal extends Animal {
         </div>
         `
     }
+    newCardMejorada(){
+        
+        const insertCard = document.querySelector(".cards");
+        const element = document.createElement ("div");
+        element.innerHTML = `
+        <div class="cardAnimal">
+            
+            <h3>
+                ${this.name}
+            </h3>
+            <img src="${this.imagen}">
+            
+            <p>
+                soy un ${this.specie} y tengo ${this.age} año(s)
+            </p>
+            <button class="buttonHambre" onclick="alert('${this.name} está comiendo')">tengo hambre</button>
+        </div>
+        `
+        insertCard.appendChild(element);
+    }
 }
